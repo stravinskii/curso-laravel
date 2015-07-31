bf859ee9ce10199dc7e1cc25cde27e7ea34d21bb
Mauricio García <mauriciogarciag@ciencias.unam.mx>
2015-07-31 11:57:21 -0500

    Setup básico de rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Declaración de ruta convencional
+Route::get('/mundo', function () {
+    return "¡Hola Mundo!";
+});
+
+// Declaración de ruta con parámetro
+Route::get('/hola/{usuario}', function ($usuario) {
+    return "¡Hola {$usuario}!";
+});
+
+// Declaración de ruta con parámetro opcional
+Route::get('/adios/{usuario?}', function ($usuario = '') {
+    return "¡Adios {$usuario}!";
+});
+
+// Declaración de ruta con reestricción de parámetros
+Route::get('/numero/{numero?}', function ($numero = null) {
+    $response = "Tu número es: %s";
+    return sprintf($response, ($numero ?: rand()));
+})
+->where(['numero' => '[0-9]+']);
+
+// Declaración de un grupo de rutas
+// Nota: Las rutas se cazan de arriba a abajo
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/libros', function () {
+        return "Esta es la sección de libros";
+    });
+
+    Route::get('/usuarios', function() {
+        return "Esta es la sección de usuarios";
+    });
+});
